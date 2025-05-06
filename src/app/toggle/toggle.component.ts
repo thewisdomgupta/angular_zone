0bc861ecb573d52dd7603b25adca494795ae0a9d
Vivek <vivekkumarguptafromktr@gmail.com>
2025-05-06 11:06:03 +0530

    toggle show code added

--- src/app/toggle/toggle.component.ts
+++ src/app/toggle/toggle.component.ts
@@ -8,8 +8,115 @@ import { Component } from '@angular/core';
 })
 export class ToggleComponent {
   toogleBox: boolean = true;
+  showCode: boolean = false;
+  copiedSection: string | null = null;
 
+  handleToggle() {
+    this.toogleBox = !this.toogleBox;
+  }
+
+  copyCode(section: 'html' | 'ts' | 'css'): void {
+    let codeToCopy = '';
+    if (section === 'html') codeToCopy = this.htmlCode;
+    else if (section === 'ts') codeToCopy = this.tsCode;
+    else if (section === 'css') codeToCopy = this.cssCode;
+
+    navigator.clipboard.writeText(codeToCopy).then(() => {
+      this.copiedSection = section;
+
+      setTimeout(() => {
+        this.copiedSection = null;
+      }, 1500);
+    }).catch(err => {
+      console.error('Failed to copy: ', err);
+    });
+  }
+
+  toggleCode() {
+    this.showCode = !this.showCode;
+  }
+
+  htmlCode: string = `
+  <div class="counter_container">
+    <h3 class="card_title">Toggle Box</h3>
+    @if(toogleBox){
+    <div class="circle_box"></div>
+    }
+    <div class="button_group">
+        <button class="plus_btn" (click)=" handleToggle()">Show</button>
+    </div>
+</div>
+  `;
+
+  tsCode: string = `
+  export class ToggleComponent {
+  toogleBox: boolean = true;
   handleToggle(){
     this.toogleBox = !this.toogleBox;
   }
+ }
+  `;
+
+  cssCode: string = `
+  .counter_container {
+    background-color: #ffffff;
+    border-radius: 12px;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+    padding: 30px;
+    width: 700px;
+    margin: 40px auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 30px;
+  }
+
+   .card_title {
+    font-size: 24px;
+    font-weight: bold;
+    margin: 0;
+    color: #333;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
+  }
+
+  .circle_box {
+    width: 180px;
+    height: 180px;
+    background: radial-gradient(circle at top left, #ff6ec4, #7873f5);
+    border-radius: 50%;
+    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 24px;
+    font-weight: bold;
+    color: white;
+    transition: opacity 0.3s ease, transform 0.3s ease;
+  }
+  
+  .button_group {
+    display: flex;
+    gap: 15px;
+  }
+  
+  .plus_btn {
+    width: 120px;
+    padding: 15px;
+    border: none;
+    border-radius: 6px;
+    color: white;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+  
+  .plus_btn {
+    background-color: #28a745;
+  }
+  
+  .plus_btn:hover {
+    background-color: #218838;
+  }
+  `;
 }
